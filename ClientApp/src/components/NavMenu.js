ce5c3e7f5f50aa87f5a5a2585e83da4957133515
HoussemMereghni <hmereghni@gmail.com>
2021-01-14 08:16:55 +0300

    FirstCommit

---
+++ ClientApp/src/components/NavMenu.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { LoginMenu } from "./api-authorization/LoginMenu";
+import "./NavMenu.css";
+import authService from "./api-authorization/AuthorizeService";
+
+export class NavMenu extends Component {
+  static displayName = NavMenu.name;
+
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true,
+      isAuthenticated: false,
+      role: null,
+    };
+  }
+  componentDidMount() {
+    this._subscription = authService.subscribe(() => this.populateState());
+    this.populateState();
+  }
+
+  componentWillUnmount() {
+    authService.unsubscribe(this._subscription);
+  }
+  async populateState() {
+    const [isAuthenticated, user] = await Promise.all([
+      authService.isAuthenticated(),
+      authService.getUser(),
+    ]);
+    this.setState({
+      isAuthenticated,
+      role: user && user.role,
+    });
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
+  render() {
+    return (
+      <header>
+        <Navbar
+          className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
+          light
+        >
+          <Container>
+            <NavbarBrand tag={Link} to="/">
+              SPA.Utility.Templates
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <Collapse
+              className="d-sm-inline-flex flex-sm-row-reverse"
+              isOpen={!this.state.collapsed}
+              navbar
+            >
+              <ul className="navbar-nav flex-grow">
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/">
+                    Home
+                  </NavLink>
+                </NavItem>
+
+                {this.state.role && this.state.role.includes("Member") ? (
+                  <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/counter">
+                      Counter
+                    </NavLink>
+                  </NavItem>
+                ) : null}
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/fetch-data">
+                    Fetch data
+                  </NavLink>
+                </NavItem>
+
+                <LoginMenu></LoginMenu>
+              </ul>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    );
+  }
+}
---
+++ Startup.cs
@@ -0,0 +1,143 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
+using SPA.Utility.Templates.Data;
+using SPA.Utility.Templates.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using System;
+using IdentityServer4.Services;
+
+namespace SPA.Utility.Templates
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>().AddRoleManager<RoleManager<IdentityRole>>()
+
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+            services.AddTransient<IProfileService, ProfileService>();
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            CreateRoles(serviceProvider).Wait();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseIdentityServer();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+        }
+        ///////
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            //initializing custom roles 
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            string[] roleNames = { "Admin", "Manager", "Member" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    //create the roles and seed them to the database: Question 1
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            //Here you could create a super user who will maintain the web app
+            var poweruser = new ApplicationUser
+            {
+
+                UserName = Configuration.GetValue<string>("AppSettings:UserName"),
+                Email = Configuration.GetValue<string>("AppSettings:UserEmail"),
+                EmailConfirmed = true,
+            };
+            //Ensure you have these values in your appsettings.json file
+            string userPWD = Configuration.GetValue<string>("AppSettings:UserPassword");
+            var _user = await UserManager.FindByEmailAsync(Configuration.GetValue<string>("AppSettings:UserEmail"));
+
+            if (_user == null)
+            {
+                var createPowerUser = await UserManager.CreateAsync(poweruser, userPWD);
+                if (createPowerUser.Succeeded)
+                {
+                    //here we tie the new user to the role
+                    await UserManager.AddToRoleAsync(poweruser, "Admin");
+
+                }
+            }
+        }
+        //////
+    }
+}
